8dcf9edb81a5397ac48f53640ad2b629a70fa131
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2024-01-29 17:49:47 +0100

    adds setUsername in authService

--- src/main/java/BWTEAM2/BW_final/services/AuthService.java
+++ src/main/java/BWTEAM2/BW_final/services/AuthService.java
@@ -43,6 +43,7 @@ public class AuthService {
         user.setPassword(bcrypt.encode(body.password()));
         user.setName(body.name());
         user.setSurname(body.surname());
+        user.setUsername(body.username());
         user.setEmail(body.email());
         user.setRole(Role.USER);
         return userDAO.save(user);
