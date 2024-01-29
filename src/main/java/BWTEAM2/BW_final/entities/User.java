c1b78a1677ad7e00f6e3b5e054b18ebdee770a33
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-29 13:00:13 +0100

    creazione security config + exception

--- src/main/java/BWTEAM2/BW_final/entities/User.java
+++ src/main/java/BWTEAM2/BW_final/entities/User.java
@@ -2,7 +2,12 @@ package BWTEAM2.BW_final.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -12,7 +17,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -26,4 +31,29 @@ public class User {
     private String avatar;
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
---
+++ src/main/java/BWTEAM2/BW_final/security/JWTAuthFilter.java
@@ -0,0 +1,50 @@
+package BWTEAM2.BW_final.security;
+
+import BWTEAM2.BW_final.entities.User;
+import BWTEAM2.BW_final.exception.UnauthorizedException;
+import BWTEAM2.BW_final.services.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class JWTAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTTools jwtTools;
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new UnauthorizedException("Please insert a valid bearer token");
+        } else {
+            String token = authHeader.substring(7);
+            jwtTools.verifyToken(token);
+            String id = jwtTools.extractIdFromToken(token);
+            User currentUser = userService.findById(UUID.fromString(id));
+            Authentication authentication = new UsernamePasswordAuthenticationToken(currentUser, null, currentUser.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
+        }
+    }
+
+        @Override
+        protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+            return new AntPathMatcher().match("/auth/**", request.getServletPath());
+        }
+
+}
