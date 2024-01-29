6b642615f78aab8e1a8166c6492b26bb17172fb6
NoemiP94 <m_emi94@hotmail.it>
2024-01-29 14:52:36 +0100

    add province features

---
+++ src/main/java/BWTEAM2/BW_final/controllers/ProvinceController.java
@@ -0,0 +1,29 @@
+package BWTEAM2.BW_final.controllers;
+
+import BWTEAM2.BW_final.entities.Province;
+import BWTEAM2.BW_final.services.ProvinceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/provinces")
+public class ProvinceController {
+    @Autowired
+    private ProvinceService provinceService;
+
+   /* @GetMapping("/{id}")
+    public Province findProvinceById(@PathVariable UUID id){
+        return provinceService.findById(id);
+    }*/
+
+    @GetMapping
+    public Page<Province> findAllProvinces(@RequestParam(defaultValue = "0") int page,
+                                      @RequestParam(defaultValue = "10") int size,
+                                      @RequestParam(defaultValue = "uuid") String orderBy){
+        return provinceService.findAllProvinces(page, size, orderBy);
+    }
+}
---
+++ src/main/java/BWTEAM2/BW_final/services/ProvinceService.java
@@ -0,0 +1,26 @@
+package BWTEAM2.BW_final.services;
+
+import BWTEAM2.BW_final.entities.Province;
+import BWTEAM2.BW_final.repositories.ProvinceDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProvinceService {
+    @Autowired
+    private ProvinceDAO provinceDAO;
+
+   /* public Province findById(UUID id){
+        return provinceDAO.findById(id).orElseThrow(()-> new NotFoundException(id));
+    }*/
+
+    public Page<Province> findAllProvinces(int page, int size, String orderBy){
+        if(size >= 50) size = 50;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(orderBy));
+        return provinceDAO.findAll(pageable);
+    }
+}
