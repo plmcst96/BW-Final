1e7c94798bab52e3f2143afe104fb322a161c227
Vincenzo Melillo <137229406+Vince-mel@users.noreply.github.com>
2024-01-29 16:28:02 +0100

    Update InvoiceService.java

--- src/main/java/BWTEAM2/BW_final/services/InvoiceService.java
+++ src/main/java/BWTEAM2/BW_final/services/InvoiceService.java
@@ -1,8 +1,13 @@
 package BWTEAM2.BW_final.services;
 
+import BWTEAM2.BW_final.entities.Client;
 import BWTEAM2.BW_final.entities.Invoice;
 import BWTEAM2.BW_final.repositories.InvoiceDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,36 +16,46 @@ import java.util.UUID;
 @Service
 public class InvoiceService {
 
-    private final InvoiceDAO invoiceDAO;
-
     @Autowired
-    public InvoiceService(InvoiceDAO invoiceDAO) {
-        this.invoiceDAO = invoiceDAO;
-    }
+    InvoiceDAO invoiceDAO;
 
     public List<Invoice> getAllInvoices() {
         return invoiceDAO.findAll();
     }
 
     public Invoice getInvoiceById(UUID uuid) {
-        return invoiceDAO.findById(uuid).orElse(null);
+        return invoiceDAO.findById(uuid).orElseThrow(() -> new NotFoundException(uuid));
     }
 
-    public Invoice saveInvoice(Invoice invoice) {
+    public Invoice saveInvoice(InvoiceDTO body) {
+        Invoice invoice = new Invoice();
+        invoice.setDate(body.getDate());
+        invoice.setAmount(body.getAmount());
+        invoice.setNumber(body.getNumber());
+        invoice.setInvoiceState(body.getInvoiceState());
+        Client client = clientService.getClientById(body.getClientId());
+        invoice.setClient(client);
         return invoiceDAO.save(invoice);
     }
 
-    public Invoice updateInvoice(UUID uuid, Invoice updatedInvoice) {
-        Invoice existingInvoice = invoiceDAO.findById(uuid).orElse(null);
-        if (existingInvoice != null) {
-            updatedInvoice.setUuid(uuid);
-            return invoiceDAO.save(updatedInvoice);
-        } else {
-            return null; 
-        }
+    public Page<Invoice> findAllInvoices(int page, int size, String orderBy) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(orderBy));
+        return invoiceDAO.findAll(pageable);
     }
 
-    public void deleteInvoice(UUID uuid) {
-        invoiceDAO.deleteById(uuid);
+    public Invoice updateInvoiceById(UUID uuid, InvoiceDTO body) {
+        Invoice invoice = invoiceDAO.findById(uuid).orElseThrow(() -> new NotFoundException(uuid));
+        invoice.setDate(body.getDate());
+        invoice.setAmount(body.getAmount());
+        invoice.setNumber(body.getNumber());
+        invoice.setInvoiceState(body.getInvoiceState());
+        Client client = clientService.getClientById(body.getClientId());
+        invoice.setClient(client);
+        return invoiceDAO.save(invoice);
+    }
+
+    public void deleteInvoiceById(UUID uuid) {
+        Invoice invoice = invoiceDAO.findById(uuid).orElseThrow(() -> new NotFoundException(uuid));
+        invoiceDAO.delete(invoice);
     }
 }
