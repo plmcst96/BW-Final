78e435ec27e4598fbd39f8b308ff38964ede7214
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-29 16:35:36 +0100

    modifiche userController

--- src/main/java/BWTEAM2/BW_final/controller/UserController.java
+++ src/main/java/BWTEAM2/BW_final/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     @GetMapping
     public Page<User> getUsers(@RequestParam(defaultValue = "0") int page,
                                @RequestParam(defaultValue = "10") int size,
-                               @RequestParam(defaultValue = "id") String orderBy) {
+                               @RequestParam(defaultValue = "uuid") String orderBy) {
         return usersService.getUsers(page, size, orderBy);
     }
 
