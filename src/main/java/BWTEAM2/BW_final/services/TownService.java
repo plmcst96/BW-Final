83f4971ab1c7d13d330d03da78a08224db525f9c
NoemiP94 <m_emi94@hotmail.it>
2024-01-29 14:36:07 +0100

    add town repository and service

---
+++ src/main/java/BWTEAM2/BW_final/services/TownService.java
@@ -0,0 +1,29 @@
+package BWTEAM2.BW_final.services;
+
+import BWTEAM2.BW_final.entities.Town;
+import BWTEAM2.BW_final.repositories.TownDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class TownService {
+    @Autowired
+    private TownDAO townDAO;
+
+   /* public Town findById(UUID id){
+        return townDAO.findById(id).orElseThrow(()-> new NotFoundException(id));
+    }*/
+
+    public Page<Town> findAllDevices(int page, int size, String orderBy){
+        if(size >= 50) size = 50;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(orderBy));
+        return townDAO.findAll(pageable);
+    }
+}
