7246ce636baf2727c1fd0a836627e872475362a5
NoemiP94 <m_emi94@hotmail.it>
2024-01-30 10:32:30 +0100

    add relations between town and province,not all town where insert into DB!

--- src/main/java/BWTEAM2/BW_final/repositories/ProvinceDAO.java
+++ src/main/java/BWTEAM2/BW_final/repositories/ProvinceDAO.java
@@ -4,8 +4,11 @@ import BWTEAM2.BW_final.entities.Province;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProvinceDAO extends JpaRepository<Province, UUID> {
+    Optional<Province> findByProvinceCode(String provinceCode);
+    Optional<Province> findByName(String name);
 }
