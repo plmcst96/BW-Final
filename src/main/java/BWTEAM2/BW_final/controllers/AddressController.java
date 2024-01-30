680241053bfccf6bd6252bf2aa2bf1b171297780
Riccardo Di Bari <riccardodibari@hotmail.it>
2024-01-30 10:32:50 +0100

    create address Controller

--- src/main/java/BWTEAM2/BW_final/controllers/AddressController.java
+++ src/main/java/BWTEAM2/BW_final/controllers/AddressController.java
@@ -1,45 +1,63 @@
 package BWTEAM2.BW_final.controllers;
 
 import BWTEAM2.BW_final.entities.Address;
+import BWTEAM2.BW_final.exception.BadRequestException;
+import BWTEAM2.BW_final.payloads.AddressDTO;
+import BWTEAM2.BW_final.payloads.AddressResponseDTO;
+import BWTEAM2.BW_final.repositories.AddressRepo;
 import  org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import BWTEAM2.BW_final.services.AddressService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/addresses")
 public class AddressController {
-
-    private final AddressService addressService;
-
     @Autowired
-    public AddressController(AddressService addressService) {
-        this.addressService = addressService;
-    }
-
-    @PostMapping
-    public Address createAddress(@RequestBody Address address) {
-        return addressService.saveAddress(address);
-    }
+    AddressService addressService;
 
     @GetMapping
-    public List<Address> getAllAddresses() {
-        return addressService.getAllAddresses();
+    public Page<Address> getAddress(@RequestParam(defaultValue = "0") int page,
+                                  @RequestParam(defaultValue = "10") int size,
+                                  @RequestParam(defaultValue = "uuid") String orderBy) {
+        return addressService.findAll(page, size, orderBy);
     }
 
     @GetMapping("/{id}")
-    public Address getAddressById(@PathVariable Long id) {
-        return addressService.getAddressById(id);
+    public Address getAddressById(@PathVariable UUID id) {
+        return addressService.findById(id);
     }
 
-    @GetMapping("/byTown/{townId}")
-    public List<Address> getAddressesByTown(@PathVariable Long townId) {
-        return addressService.getAddressesByTown(townId);
+    @PutMapping("/{id}")
+    //@PreAuthorize("hasAuthority('ADMIN')")
+    public Address getAddressByIdAndUpdate(@PathVariable UUID id, @RequestBody AddressDTO addressPayload) {
+        return addressService.updateById(id, addressPayload);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteAddress(@PathVariable Long id) {
-        addressService.deleteAddress(id);
+    //@PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void getAddressByIdAndDelete(@PathVariable UUID id) {
+        addressService.deleteById(id);
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    // @PreAuthorize("hasAuthority('ADMIN')")
+
+    public AddressResponseDTO create(@RequestBody @Validated AddressDTO address, BindingResult validation) {
+        if(validation.hasErrors()) {
+            System.out.println(validation.getAllErrors());
+            throw new BadRequestException("Something is wrong in the payload.");
+        } else {
+            Address newAddress = addressService.save(address);
+            return new AddressResponseDTO(newAddress.getUuid());
+        }
     }
 }
